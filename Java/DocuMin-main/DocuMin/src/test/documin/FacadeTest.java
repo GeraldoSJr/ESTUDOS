3dc99630150fc36d554e4f4b2e9aa317b4702c86
Geraldo157 <gsmecanica76@gmail.com>
2023-05-24 14:53:34 -0300

    Adicionando LP5

---
+++ Java/DocuMin-main/DocuMin/src/test/documin/FacadeTest.java
@@ -0,0 +1,225 @@
+package test.documin; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Facade Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>mai 23, 2023</pre> 
+* @version 1.0 
+*/ 
+public class FacadeTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: criarDocumento(String titulo) 
+* 
+*/ 
+@Test
+public void testCriarDocumentoTitulo() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: criarDocumento(String titulo, int tamanhoMaximo) 
+* 
+*/ 
+@Test
+public void testCriarDocumentoForTituloTamanhoMaximo() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: removerDocumento(String titulo) 
+* 
+*/ 
+@Test
+public void testRemoverDocumento() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: contarElementos(String titulo) 
+* 
+*/ 
+@Test
+public void testContarElementos() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: exibirDocumento(String titulo) 
+* 
+*/ 
+@Test
+public void testExibirDocumento() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: criarTexto(String tituloDoc, String valor, int prioridade) 
+* 
+*/ 
+@Test
+public void testCriarTexto() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: criarTitulo(String tituloDoc, String valor, int prioridade, int nivel, boolean linkavel) 
+* 
+*/ 
+@Test
+public void testCriarTitulo() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: criarLista(String tituloDoc, String valorLista, int prioridade, String separador, String caractere) 
+* 
+*/ 
+@Test
+public void testCriarLista() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: criarTermos(String tituloDoc, String valorTermos, int prioridade, String separador, String ordem) 
+* 
+*/ 
+@Test
+public void testCriarTermos() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: pegarRepresentacaoCompleta(String tituloDoc, int elementoPosicao) 
+* 
+*/ 
+@Test
+public void testPegarRepresentacaoCompleta() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: pegarRepresentacaoResumida(String tituloDoc, int elementoPosicao) 
+* 
+*/ 
+@Test
+public void testPegarRepresentacaoResumida() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: apagarElemento(String tituloDoc, int elementoPosicao) 
+* 
+*/ 
+@Test
+public void testApagarElemento() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: moverParaCima(String tituloDoc, int elementoPosicao) 
+* 
+*/ 
+@Test
+public void testMoverParaCima() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: moverParaBaixo(String tituloDoc, int elementoPosicao) 
+* 
+*/ 
+@Test
+public void testMoverParaBaixo() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: criarAtalho(String tituloDoc, String tituloDocReferenciado) 
+* 
+*/ 
+@Test
+public void testCriarAtalho() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: criarVisaoCompleta(String tituloDoc) 
+* 
+*/ 
+@Test
+public void testCriarVisaoCompleta() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: criarVisaoResumida(String tituloDoc) 
+* 
+*/ 
+@Test
+public void testCriarVisaoResumida() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: criarVisaoPrioritaria(String tituloDoc, int prioridade) 
+* 
+*/ 
+@Test
+public void testCriarVisaoPrioritaria() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: criarVisaoTitulo(String tituloDoc) 
+* 
+*/ 
+@Test
+public void testCriarVisaoTitulo() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: exibirVisao(int visaold) 
+* 
+*/ 
+@Test
+public void testExibirVisao() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
---
+++ Java/DocuMin-main/DocuMin/src/visoes/visaoResumida.java
@@ -0,0 +1,31 @@
+package visoes;
+
+import java.util.ArrayList;
+
+import documin.documento.Documento;
+import documin.elementos.Elemento;
+
+/**
+ * 
+ * @author Geraldo Sobreira Júnior
+ *
+ */
+public class visaoResumida extends visao {
+
+	public visaoResumida(Documento documento) {
+		super(documento);
+	}
+
+	@Override
+	public String[] exibeVisao() {
+		String[] retorno = new String[documento.contaElementos()];
+		ArrayList<Elemento> elementos = documento.getElementos();
+		
+		for (int i = 0; i < retorno.length; i++) {
+			retorno[i] = elementos.get(i).representacaoResumida();
+		}
+		
+		return retorno;
+	}
+
+}
